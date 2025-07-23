58b65e7b8b81b20549f4f315fad748ab8d825830
SolomonLihali <slyhero0@gmail.com>
2025-07-23 07:47:29 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
-const landlordRoutes = require("./routes/landlord");
 const express = require("express");
 const cors = require("cors");
+const landlordRoutes = require("./routes/landlord");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/landlord", landlordRoutes);
+
 app.get("/", (req, res) => res.send("Backend is running"));
+
 app.listen(PORT, () => console.log("Server running on port", PORT));
